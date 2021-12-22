abd314974684f40f49b0ffa9909575fa1cf8c999
Gage Bachik <loser@gagebachik.com>
2021-12-21 23:02:41 -0800

    working tests added

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -1,17 +1,37 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { SolanaTwitter } from '../target/types/solana_twitter';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { SolanaTwitter } from "../target/types/solana_twitter";
 
-describe('solana-twitter', () => {
+import * as assert from "assert";
 
+// Begin custom tests
+
+it("can send a new tweet", async () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
-
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
-
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });
+  const tweet = anchor.web3.Keypair.generate();
+  await program.rpc.sendTweet(
+    "SolanaTwitter",
+    "Hello, world! This is the first tweet on my local solana cluster.",
+    {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweet],
+    }
+  );
+  const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+  assert.equal(
+    tweetAccount.author.toBase58(),
+    program.provider.wallet.publicKey.toBase58()
+  );
+  assert.equal(tweetAccount.topic, "SolanaTwitter");
+  assert.equal(
+    tweetAccount.content,
+    "Hello, world! This is the first tweet on my local solana cluster."
+  );
+  assert.ok(tweetAccount.timestamp);
 });
